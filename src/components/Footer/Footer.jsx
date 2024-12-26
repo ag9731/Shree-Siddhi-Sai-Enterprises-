82fba8553852775613fe4d2f81a2f6b30c24719a
Akash <goravanakollakash@gmail.com>
2024-12-26 15:35:11 +0530

    second commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,257 +1,110 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
   faInstagram,
   faTwitter,
   faWhatsapp,
 } from "@fortawesome/free-brands-svg-icons";
-// import footerLogo from "../../assets/logo-removebg-preview.png"
-import {
-  faEnvelopeOpen,
-  faFileAlt,
-  faFileArchive,
-  faMapMarkedAlt,
-  faMapMarker,
-  faMobile,
-  faMobileAlt,
-} from "@fortawesome/free-solid-svg-icons";
-import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons/faMapMarkerAlt";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../../assets/Logo (2).png"
-import "./Footer.css"
-import AboutDropdown from "../Nav/AboutDropdpwn/AboutDropDown";
+import { faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
+import logo from "../../assets/Logo (2).png";
+
 const Footer = () => {
   const handleClick = () => {
-    window.scrollTo({ behavior: 'smooth', top: "0" });
+    window.scrollTo({ behavior: "smooth", top: 0 });
   };
+
   return (
-    <div className="footer bg-zinc-950 py-8">
-      {/* Newslatter and Columns */}
-      <div
-        className=" bg-black bg-opacity-50 absolute top-0 left-0 p-4
-      h-full "
-      >
-        {/* Footer Columns */}
-        <div className="footer-columns p-10">
-          <div className="flex flex-col lg:flex-row md:flex-row ">
-            {/* Column */}
-            <div className="w-full sm:w-1/3 mb-8 sm:mb-0 text-left">
-              <div className="-my-12 text-white">
-                <div className="font-bold flex flex-row items-center justify-center">
-                  <img src={logo} className="w-[150px] h-[150px] py-2" />
-                </div>
-                <p className="text-justify">
-                  Established in 1994, Shree Chethan Services has been providing
-                  our clients with excellent services in Karnataka and across
-                  India.
-                </p>
-                <div className="social-icons mt-4">
-                  <ul className="flex space-x-3 justify-center">
-                    <li>
-                      <FontAwesomeIcon
-                        icon={faFacebook}
-                        className="text-white text-3xl
-                    hover:text-amber-400 transition ease-in-out duration-300"
-                      />
-                    </li>
-                    <li>
-                      <FontAwesomeIcon
-                        icon={faInstagram}
-                        className="text-white text-3xl
-                    hover:text-amber-400 transition ease-in-out duration-300"
-                      />
-                    </li>
-                    <li>
-                      <FontAwesomeIcon
-                        icon={faTwitter}
-                        className="text-white text-3xl
-                    hover:text-amber-400 transition ease-in-out duration-300"
-                      />
-                    </li>
-                    <li>
-                      <FontAwesomeIcon
-                        icon={faWhatsapp}
-                        className="text-white text-3xl
-                    hover:text-amber-400 transition ease-in-out duration-300"
-                      />
-                    </li>
-                  </ul>
-                </div>
-                <br />
-                <div className="address-list mt-2">
-                  <ul className="flex justify-center text-justify items-center">
-                    <li className="flex items-center -my-4 ">
-                      <FontAwesomeIcon
-                        icon={faEnvelopeOpen}
-                        className="text-amber-400 
-                   text-xl p-4"
-                      />
-                      <a
-                        href="mailto:admissions@hillsideacademy.in"
-                        className="text-white"
-                      >
-                        chethanaservices@gmail.com
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+    <footer className="bg-zinc-950 text-white py-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {/* Column 1 */}
+          <div className="flex flex-col items-center md:items-start">
+            <img src={logo} alt="Logo" className="w-24 h-24 mb-4" />
+            <p className="text-center md:text-left mb-4">
+              Established in 1994, Shree Chethan Services has been providing our
+              clients with excellent services in Karnataka and across India.
+            </p>
+            <div className="flex space-x-4 mb-4">
+              <FontAwesomeIcon
+                icon={faFacebook}
+                className="text-2xl hover:text-amber-400 transition duration-300"
+              />
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="text-2xl hover:text-amber-400 transition duration-300"
+              />
+              <FontAwesomeIcon
+                icon={faTwitter}
+                className="text-2xl hover:text-amber-400 transition duration-300"
+              />
+              <FontAwesomeIcon
+                icon={faWhatsapp}
+                className="text-2xl hover:text-amber-400 transition duration-300"
+              />
             </div>
-            {/* Column */}
-            {/* Column */}
-            <div className="w-full sm:w-1/3 my-8 lg:my-0 md:my-0 mb-12 ">
-              <div className="footer-c-widget h-white list-widget text-white">
-                <h3 className="text-lg font-semibold mb-4 flex justify-center">
-                  QUICK LINKS
-                </h3>
-                <ul className="flex flex-col items-center justify-center">
-                  <li className="relative">
-                    <Link
-                      onClick={handleClick}
-                      to={"/"}
-                      className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
-                    >
-                      Home
-                    </Link>
-                  </li>
-                  <li className="relative">
-                    <Link
-                      onClick={handleClick}
-                      to={"/aboutUs"}
-                      className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
-                    >
-                      About Us
-                    </Link>
-                    {/* <AboutDropdown/> */}
-                  </li>
-                  <li className="relative">
-                    <Link
-                      onClick={handleClick}
-                      to={"/project"}
-                      className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
-                    >
-                      Project
-                    </Link>
-                  </li>
-                  <li className="relative">
-                    <Link
-                      onClick={handleClick}
-                      to={"/client"}
-                      className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
-                    >
-                      Clientele
-                    </Link>
-                  </li>
-                  {/* <li className="relative">
-                    <Link onClick={ handleClick} to={"/services"}
-                      className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
-                    >
-                     Services
-                    </Link>
-                  </li> */}
+            <div className="flex items-center">
+              <FontAwesomeIcon
+                icon={faEnvelopeOpen}
+                className="text-amber-400 mr-2"
+              />
+              <a
+                href="mailto:chethanaservices@gmail.com"
+                className="hover:underline"
+              >
+                chethanaservices@gmail.com
+              </a>
+            </div>
+          </div>
 
-                  <li className="relative ">
+          {/* Column 2 */}
+          <div className="text-center md:text-left">
+            <h3 className="text-lg font-semibold mb-4">QUICK LINKS</h3>
+            <ul className="space-y-2">
+              {["Home", "About Us", "Project", "Clientele", "Contact Us"].map(
+                (item) => (
+                  <li key={item}>
                     <Link
                       onClick={handleClick}
-                      to={"/contact"}
-                      className="block mb-2 after:content-[''] after:absolute
-                       after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
+                      to={`/${item.toLowerCase().replace(" ", "")}`}
+                      className="hover:text-amber-400 transition duration-300"
                     >
-                      Contact Us
+                      {item}
                     </Link>
                   </li>
-                </ul>
-              </div>
-            </div>
-            <div className="w-full sm:w-1/3  lg:my-0 md:my-0 ">
-              <div className="footer-c-widget h-white list-widget text-white">
-                <h3 className="text-lg font-semibold text-center mb-4 mx-14">
-                  SERVICES
-                </h3>
-                <ul className="flex flex-col items-center justify-center">
-                  <li className="relative">
-                    <Link
-                      onClick={handleClick}
-                      to={"/excavation"}
-                      className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
-                    >
-                      Excavation Services
-                    </Link>
-                  </li>
-                  <li className="relative">
-                    <Link
-                      onClick={handleClick}
-                      to={"/demolition"}
-                      className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
-                    >
-                      Demolition/Dismantling Services
-                    </Link>
-                  </li>
-                  <li className="relative">
-                    <Link
-                      onClick={handleClick}
-                      to={"/siteprep"}
-                      className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
-                    >
-                      Site Preparations
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="w-full my-6 md:my-0 sm:w-1/3 mb-8 sm:mb-0">
-              <div className="footer-c-widget h-white address-list text-white">
-                <h3 className="text-lg md:text-center text-center  font-semibold mb-4">
-                  CONTACT US
-                </h3>
-                <ul className="text-left">
-                  <li className="flex flex-col md:flex-row justify-center text-center md:text-justify  items-center gap-y-3 mb-4">
-                    <FontAwesomeIcon
-                      icon={faMapMarkerAlt}
-                      className="text-amber-400 text-xl px-4"
-                    />{" "}
-                    #637, Muniyappa complex, jalahalli cross circle, peenya,
-                    bengaluru- 560057
-                  </li>
-                  <hr className="border-gray-700 mb-4" />
-                  <h5 className="text-md text-center  md:text-center font-semibold mb-2">
-                    PHONE NO
-                  </h5>
-                  <li className="flex flex-col md:flex-row items-center gap-y-3 justify-center mb-4">
-                    <FontAwesomeIcon
-                      icon={faMobileAlt}
-                      className="text-amber-400 text-xl px-4"
-                    />{" "}
-                    +91-9448334376
-                  </li>
+                )
+              )}
+            </ul>
+          </div>
 
-                  <hr className="border-gray-700 mb-4" />
-                  {/* <li className="flex items-start">
-                    <FontAwesomeIcon
-                      icon={faFileArchive}
-                      className="text-amber-400 text-xl px-4"
-                    />{" "}
-                    <a
-                      href="privacy-policy.html"
-                      className="text-white text-center"
-                    >
-                      Privacy Policy
-                    </a>
-                  </li> */}
-                </ul>
-              </div>
-            </div>
-            {/* Column */}
+          {/* Column 3 */}
+          <div className="text-center md:text-left">
+            <h3 className="text-lg font-semibold mb-4">SERVICES</h3>
+            <ul className="space-y-2">
+              {[
+                { name: "Excavation Services", path: "/excavation" },
+                {
+                  name: "Demolition/Dismantling Services",
+                  path: "/demolition",
+                },
+                { name: "Site Preparations", path: "/siteprep" },
+              ].map((service) => (
+                <li key={service.name}>
+                  <Link
+                    onClick={handleClick}
+                    to={service.path}
+                    className="hover:text-amber-400 transition duration-300"
+                  >
+                    {service.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
-      {/* Sub Footer */}
-      {/* <div className="sub-footer bg-gray-900 text-white text-center py-4">
-        <p className="text-sm">Powered by INNOMATRICS TECH.</p>
-      </div> */}
-      {/* Sub Footer */}
-    </div>
+    </footer>
   );
 };
 
--- src/components/Services/DemolitionDismantle/DemolitionDismantle.jsx
+++ src/components/Services/DemolitionDismantle/DemolitionDismantle.jsx
@@ -38,7 +38,7 @@ const DemolitionDismantle = () => {
   return (
     <div className="bg-gray-50">
       {/* <DemolitionHero /> */}
-      <DemolitionText />
+      {/* <DemolitionText /> */}
       
       <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
         <div className="text-center mb-12">
